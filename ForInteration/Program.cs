6c7848cd057ca307e4e317ab3d86e4cc8a9cecaa
Taj <tajmarie92@gmail.com>
2022-07-22 20:58:57 -0500

    Add project files.

---
+++ ForInteration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForInteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+                Console.ReadLine();
+
+        }
+    }
+}
